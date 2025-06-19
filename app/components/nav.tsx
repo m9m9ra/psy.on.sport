678ccd62954d7447e67de17a11a51a88499eed61
m9m9ra <vasa4g@gmail.com>
2025-06-19 14:57:48 +0200

    change

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -3,7 +3,8 @@
 import { ArrowLeft, ChevronLeft, X } from "lucide-react";
 import Link from "next/link";
 import React, { useEffect, useRef, useState } from "react";
-import '../sass/nav.css';
+// import '../sass/nav.css';
+import styles from './../sass/nav.module.scss';
 import Image from "next/image";
 
 type props = {
@@ -31,26 +32,26 @@ export const Navigation: React.FC = ({ canGoBack }: props) => {
 
 	return (
 		<>
-			<header ref={ref} className="navbar">
-				<div className="layout">
+			<header ref={ref} className={styles.navbar}>
+				<div className={styles.layout}>
 					<Image src='favicon.svg' width={50} height={50} alt="" />
-					<div className="middle">
+					<div className={styles.middle}>
 						<Link href={`/`}>
-							<p className="active">Главная</p>
+							<p className={styles.active}>Главная</p>
 						</Link>
 						<Link href={`/`}><p>О проекте</p></Link>
 						<Link href={`/`}><p>Наша команда</p></Link>
 						<Link href={`/`}><p>Здесь о полезном</p></Link>
 					</div>
-					<div onClick={toggleBurger} className="burger">
+					<div onClick={toggleBurger} className={styles.burger}>
 						<div></div>
 						<div></div>
 						<div></div>
 					</div>
 				</div>
 				{burger ?
-					<div className="menu" >
-						<div className="close">
+					<div className={styles.menu} >
+						<div className={styles.close}>
 							<X onClick={toggleBurger} />
 						</div>
 						<Link href={`/`}>
